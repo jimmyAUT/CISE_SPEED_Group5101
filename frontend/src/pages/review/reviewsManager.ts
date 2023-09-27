ec98815a57a506f090a36cc835a21adcd2f397bc
MiraclezzzS <131764763+MiraclezzzS@users.noreply.github.com>
2023-09-27 15:44:12 +1300

    Try some updates of reviewsManager.ts

--- frontend/src/pages/review/reviewsManager.ts
+++ frontend/src/pages/review/reviewsManager.ts
@@ -1,1 +1,9 @@
+import { ArticleInterface } from "./index";
 
+let reviews: ArticleInterface[] = [];
+
+export const getReviews = (): ArticleInterface[] => reviews;
+
+export const addReview = (article: ArticleInterface) => {
+    reviews.push(article);
+};
